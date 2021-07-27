10a0c512eff1d0a3a5bc27b3d0f3213d14a9686f
KelvenH <kelvenhughes@me.com>
2021-07-27 02:29:20 +0000

    added registration form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,11 @@ mongo = PyMongo(app)
 def get_foods():
     foods = mongo.db.foods.find()
     return render_template("foods.html", foods=foods)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
