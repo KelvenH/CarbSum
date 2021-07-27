fa9ce936db16cc53bae4407ccee377f28db9e9d7
KelvenH <kelvenhughes@me.com>
2021-07-27 00:49:40 +0000

    added nav and initial styles

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
 
 mongo = PyMongo(app)
 
