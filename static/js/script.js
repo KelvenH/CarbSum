6e4c9db37c4b821e91d3b2490824853bcbc09920
KelvenH <kelvenhughes@me.com>
2021-08-02 23:31:26 +0000

    added 3 further items to quick calc

--- static/js/script.js
+++ static/js/script.js
@@ -17,6 +17,8 @@ $(document).ready(function () {
 /* --Defect : despite setting input step value to 2 decimal places, calculated results can still display float to multiple decimal places
 Status : Resolved - '.toFixed(2)' added to float being parsed to limit to 2 decimal places */
 
+//original calcs by ID need to changce to be reuseable
+
 function carbPerGram() {
   let baseWeight = parseFloat(document.getElementById("qc-base_weight").value, 10);
   let baseCarbs = parseFloat(document.getElementById("qc-base_carbs").value, 10);
@@ -34,9 +36,31 @@ function qcItemCarbs() {
   document.getElementById("qc-item-carbs-big").value = parseFloat(result).toFixed(2);
 }
 
+//Update Item Count
+
+// dependent on resolving the recreation of the id based functions
 
 
 
 
 
- 
+//Recreated to work for item 1
+
+function carbPerGram1() {
+  let baseWeight = parseFloat(document.getElementById("qc-base_weight1").value, 10);
+  let baseCarbs = parseFloat(document.getElementById("qc-base_carbs1").value, 10);
+  let result = baseCarbs / baseWeight;
+  document.getElementById("qc-carb-per-g1").value = parseFloat(result).toFixed(2);
+}
+
+
+//Calculate portion carbs
+
+function qcItemCarbs1() {
+  let portionSize = parseFloat(document.getElementById("qc-portion_weight1").value, 10);
+  let carbsPerG = parseFloat(document.getElementById("qc-carb-per-g1").value, 10);
+  let result = portionSize * carbsPerG;
+  document.getElementById("qc-item-carbs1").value = parseFloat(result).toFixed(2);
+  document.getElementById("qc-item1-carbs-big").value = parseFloat(result).toFixed(2);
+}
+
